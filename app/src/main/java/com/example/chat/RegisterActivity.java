12389ad6ac39d0cffde2130f616454691f9cddc0
Sasha Ustychenko <oustichenko04@gmail.com>
2024-04-07 00:49:04 +0300

    added registering of users

--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -1,24 +1,111 @@
 package com.example.chat;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
+    EditText mEmailEt,mPasswordEt;
+    Button mRegisterBtn;
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        mEmailEt = findViewById(R.id.emailEt);
+        mPasswordEt = findViewById(R.id.passwordEt);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering User...");
+
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPasswordEt.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    mEmailEt.setError("Invalid email");
+                    mEmailEt.setFocusable(true);
+                }
+                else if (password.length()<6)
+                {
+                    mPasswordEt.setError("Password length at least 6 characters");
+                    mPasswordEt.setFocusable(true);
+                }
+                else
+                {
+                    registerUser(email,password);
+                }
+
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void registerUser(String email, String password) {
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
